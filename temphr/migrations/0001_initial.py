2e777b22fa96f4ed8a5d01176bac56bc909a0f65
hadi <elzayn@tgggroup.com>
2015-07-27 16:29:39 -0500

    made candidate and skill models, migrated database

---
+++ temphr/migrations/0001_initial.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Candidate',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('rating', models.DecimalField(max_digits=3, decimal_places=2)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Skill',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('user', models.ForeignKey(to='temphr.Candidate')),
+            ],
+        ),
+    ]
--- temphr/models.py
+++ temphr/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Candidate(models.Model):
+	name = models.CharField(max_length=200)
+	rating = models.DecimalField(decimal_places=2, max_digits=3)
+
+class Skill(models.Model):
+	name = models.CharField(max_length=200)
+	user = models.ForeignKey(Candidate)
