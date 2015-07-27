b954e271612a56eca95363a4f3430b23abd1c28f
hadi <elzayn@tgggroup.com>
2015-07-27 16:40:04 -0500

    fixed str bug, added skill to admin register

--- temphr/admin.py
+++ temphr/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Candidate
+from .models import Candidate, Skill
 # Register your models here.
 admin.site.register(Candidate)
+admin.site.register(Skill)
--- temphr/models.py
+++ temphr/models.py
@@ -1,11 +1,17 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 class Candidate(models.Model):
 	name = models.CharField(max_length=200)
 	rating = models.DecimalField(decimal_places=2, max_digits=3)
-
+	join_date = models.DateTimeField(default = timezone.now)
+	
+	def join(self):
+		self.join_date = timezone.now()
+		self.save()
+	def __str__(self):
+		return self.name
 class Skill(models.Model):
 	name = models.CharField(max_length=200)
 	user = models.ForeignKey(Candidate)
