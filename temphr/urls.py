c0dcf9e4637c9ccb63ed593087696ee0c3daf6fa
hadi <elzayn@tgggroup.com>
2015-07-29 14:11:43 -0500

    added candidate profile template

--- temphr/urls.py
+++ temphr/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-	url(r'^$', views.candidate_list, name='candidate_list')
+	url(r'^$', views.candidate_list, name='candidate_list'),
+	url(r'^candidate_profile/(?P<pk>[0-9]+)/$', views.candidate_profile, name = 'candidate_profile')
 ]
--- temphr/views.py
+++ temphr/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Candidate, Skill
 from django.utils import timezone
 # Create your views here.
 def candidate_list(request):
 	candidates = Candidate.objects.filter(join_date__lte=timezone.now()).order_by('join_date')
 	return render(request, 'temphr/candidate_list.html',{'candidates': candidates})
+
+def candidate_profile(request,pk):
+	cand = get_object_or_404(Candidate,pk=pk)
+	return render(request, 'temphr/candidate_profile.html',{'candidate': cand})
